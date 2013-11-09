98793c12adbc05f9ba3dbd875a0fed6017aeaf13
David Froehlich <semperhilaris@gmail.com>
2013-11-09 11:19:22 +0100

    Updated debug renderer to reflect changes in libGDX 0.9.9

--- src/com/semperhilaris/smoothcam/SmoothCamDebugRenderer.java
+++ src/com/semperhilaris/smoothcam/SmoothCamDebugRenderer.java
@@ -21,7 +21,7 @@ public class SmoothCamDebugRenderer {
 	public void render (SmoothCamWorld world, Matrix4 projMatrix) {
 		SmoothCamPoint[] points = world.getPoints();
 		renderer.setProjectionMatrix(projMatrix);
-		renderer.begin(ShapeType.Circle);
+		renderer.begin(ShapeType.Line);
 		for (int i = 0; i < points.length; i++) {
 			renderer.setColor(0, 1, 0, 1);
 			renderer.circle(points[i].getX(), points[i].getY(), points[i].getInnerRadius());
@@ -35,15 +35,12 @@ public class SmoothCamDebugRenderer {
 		renderer.setColor(0, 0, 0, 1);
 		SmoothCamSubject subject = world.getSubject();
 		renderer.circle(subject.getX(), subject.getY(), subject.getVelocityRadius());
-		renderer.end();
+		renderer.circle(subject.getX(), subject.getY(), subject.getVelocityRadius() + subject.getAimingRadius());
 		if (world.getBoundingBox().w > 0 && world.getBoundingBox().h > 0) {
-			renderer.begin(ShapeType.Box);
 			renderer.setColor(1, 0, 0, 1);
 			renderer.box(world.getX() - world.getBoundingBox().w / 2, world.getY() - world.getBoundingBox().h / 2, 0,
 				world.getBoundingBox().w, world.getBoundingBox().h, 0);
-			renderer.end();
 		}
-		renderer.begin(ShapeType.Line);
 		renderer.setColor(0, 0, 0, 1);
 		renderer.line(world.getX() + 3f, world.getY(), world.getX() - 3f, world.getY());
 		renderer.line(world.getX(), world.getY() + 3f, world.getX(), world.getY() - 3f);
